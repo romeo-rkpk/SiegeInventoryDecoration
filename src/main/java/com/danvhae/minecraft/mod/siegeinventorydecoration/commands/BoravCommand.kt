7aa38e7ba376aaa3e0364eeab5a3b9786d05c6e3
detDaye <mindaye8504@gmail.com>
2022-12-20 21:37:06 +0900

    Feat: 보라비 명령어 작성됨

--- src/main/java/com/danvhae/minecraft/mod/siegeinventorydecoration/commands/BoravCommand.kt
+++ src/main/java/com/danvhae/minecraft/mod/siegeinventorydecoration/commands/BoravCommand.kt
@@ -1,4 +1,21 @@
 package com.danvhae.minecraft.mod.siegeinventorydecoration.commands
 
-class BoravCommand {
+import com.danvhae.minecraft.mod.siegeinventorydecoration.SiegeInventoryDecoration
+import com.danvhae.minecraft.mod.siegeinventorydecoration.abstracts.InventoryCommandAbstract
+import com.danvhae.minecraft.mod.siegeinventorydecoration.handlers.ModGUIHandler
+import net.minecraft.command.ICommandSender
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.server.MinecraftServer
+
+class BoravCommand : InventoryCommandAbstract() {
+    override fun getName(): String {
+        return "borav"
+    }
+
+    override fun execute(server: MinecraftServer, sender: ICommandSender, args: Array<out String>) {
+        val player = sender as? EntityPlayer ?:return
+        player.openGui(SiegeInventoryDecoration.INSTANCE, ModGUIHandler.BORAV,
+            player.world, player.posX.toInt(), player.posY.toInt(), player.posZ.toInt()
+        )
+    }
 }
