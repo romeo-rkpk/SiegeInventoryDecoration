bb895a893622a543ea1242cc16bf358118aea7c3
detDaye <mindaye8504@gmail.com>
2022-12-22 07:41:26 +0900

    Feat: 단츄 GUI컨테이너 추가

--- src/main/java/com/danvhae/minecraft/mod/siegeinventorydecoration/container/DanchuGUIContainer.kt
+++ src/main/java/com/danvhae/minecraft/mod/siegeinventorydecoration/container/DanchuGUIContainer.kt
@@ -1,4 +1,12 @@
 package com.danvhae.minecraft.mod.siegeinventorydecoration.container
 
-class DanchuGUIContainer {
+import com.danvhae.minecraft.mod.siegeinventorydecoration.SiegeInventoryDecoration
+import com.danvhae.minecraft.mod.siegeinventorydecoration.abstracts.ContainerGUIAbstract
+import net.minecraft.inventory.Container
+import net.minecraft.util.ResourceLocation
+
+class DanchuGUIContainer(inventorySlotsIn: Container) : ContainerGUIAbstract(inventorySlotsIn){
+    override fun texture(): ResourceLocation {
+        return ResourceLocation(SiegeInventoryDecoration.MOD_ID, "danchu.png")
+    }
 }
