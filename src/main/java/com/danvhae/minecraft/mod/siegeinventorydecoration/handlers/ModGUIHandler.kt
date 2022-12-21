8b9ab4782bccd7af3578605d135eabb14117cf52
detDaye <mindaye8504@gmail.com>
2022-12-22 07:44:32 +0900

    Feat: GUI 핸들러에 단츄 해야 거 추가

--- src/main/java/com/danvhae/minecraft/mod/siegeinventorydecoration/handlers/ModGUIHandler.kt
+++ src/main/java/com/danvhae/minecraft/mod/siegeinventorydecoration/handlers/ModGUIHandler.kt
@@ -1,6 +1,8 @@
 package com.danvhae.minecraft.mod.siegeinventorydecoration.handlers
 
 import com.danvhae.minecraft.mod.siegeinventorydecoration.container.BoravGUIContainer
+import com.danvhae.minecraft.mod.siegeinventorydecoration.container.DanchuGUIContainer
+import com.danvhae.minecraft.mod.siegeinventorydecoration.container.HaeyaGuiContainer
 import com.danvhae.minecraft.mod.siegeinventorydecoration.container.SimpleContainer
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.entity.player.EntityPlayer
@@ -11,18 +13,22 @@ import net.minecraftforge.fml.common.network.IGuiHandler
 class ModGUIHandler : IGuiHandler{
 
     companion object{
+        const val HAEYA = 1
         const val BORAV = 2
+        const val DANCHU = 3
     }
     override fun getServerGuiElement(ID: Int, player: EntityPlayer?, world: World?, x: Int, y: Int, z: Int): Container? {
         return when(ID){
-            BORAV -> SimpleContainer(player?.inventory?:return null)
+            DANCHU, BORAV, HAEYA -> SimpleContainer(player?.inventory?:return null)
             else -> null
         }
     }
 
     override fun getClientGuiElement(ID: Int, player: EntityPlayer?, world: World?, x: Int, y: Int, z: Int): GuiContainer? {
         return when(ID){
+            DANCHU -> DanchuGUIContainer(getServerGuiElement(ID, player, world, x, y, z)!!)
             BORAV -> BoravGUIContainer(getServerGuiElement(ID, player, world, x, y, z)!!)
+            HAEYA -> HaeyaGuiContainer(getServerGuiElement(ID, player, world, x, y, z)!!)
             else -> null
         }
     }
